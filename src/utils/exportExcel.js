0e0afdab08bdaae15a11b26c853ad76fbb091c7a
luaryshop <luarysemijoias@gmail.com>
2026-07-21 20:42:49 -0300

    Delete src directory

--- src/utils/exportExcel.js
+++
@@ -1,8 +0,0 @@
-import * as XLSX from "xlsx";
-
-export const exportarExcel = (dados, nome = "dados.xlsx") => {
-  const ws = XLSX.utils.json_to_sheet(dados);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
-  XLSX.writeFile(wb, nome);
-};
